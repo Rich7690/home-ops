e0cfbfd3d150433d529cce598c5a27a73fe377cc
Rich <11451714+Rich7690@users.noreply.github.com>
2022-08-20 23:04:50 -0700

    latest updates

--- terraform/metrics.tf
+++ terraform/metrics.tf
@@ -1,23 +1,4 @@
-resource "helm_release" "vm" {
-  name        = "vm"
-  chart       = "victoria-metrics-single"
-  repository  = "https://victoriametrics.github.io/helm-charts/"
-  version     = "0.8.34"
-  max_history = 3
-
-  set {
-    name  = "server.scrape.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "server.persistentVolume.existingClaim"
-    value = "vm"
-  }
-
-  values = [templatefile("./prometheus.yml", { external_domain = var.external_domain, ping_id = var.ping_id })]
-}
-
+//TODO: move to argo
 resource "kubernetes_persistent_volume_claim_v1" "vm" {
   metadata {
     name = "vm"

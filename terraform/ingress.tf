497168fec615c2a143380c48b0ffaa5c4c313891
Rich <11451714+Rich7690@users.noreply.github.com>
2023-02-03 07:06:40 +0000

    cleanup

--- terraform/ingress.tf
+++ terraform/ingress.tf
@@ -1,39 +1,3 @@
-resource "kubernetes_ingress_v1" "loki" {
-  metadata {
-    name = "loki"
-    annotations = {
-      "nginx.ingress.kubernetes.io/service-upstream" = "true"
-    }
-  }
-  spec {
-    ingress_class_name = "nginx"
-    tls {
-      hosts       = ["*.${var.domain_name}", "${var.domain_name}"]
-      secret_name = "prod-cert"
-    }
-    rule {
-      host = "loki.${var.domain_name}"
-      http {
-
-        path {
-          path_type = "ImplementationSpecific"
-          backend {
-            service {
-              name = "loki"
-              port {
-                name = "http-metrics"
-              }
-            }
-          }
-          path = "/"
-        }
-      }
-    }
-  }
-
-}
-
-
 resource "kubernetes_ingress_v1" "argo_CD" {
   metadata {
     name = "argo-cd"
@@ -99,43 +63,6 @@ resource "kubernetes_ingress_v1" "iotawatt" {
           backend {
             service {
               name = "iotawatt"
-              port {
-                name = "http"
-              }
-            }
-          }
-          path = "/"
-        }
-      }
-    }
-  }
-
-}
-
-resource "kubernetes_ingress_v1" "theila" {
-  metadata {
-    name = "theila"
-    annotations = {
-      #"nginx.ingress.kubernetes.io/auth-signin"            = "https://${var.domain_name}/oauth2/sign_in"
-      #"nginx.ingress.kubernetes.io/auth-url"               = "https://${var.domain_name}/oauth2/auth"
-      "nginx.ingress.kubernetes.io/whitelist-source-range" = "192.168.0.0/16,10.0.0.0/8"
-    }
-  }
-  spec {
-    ingress_class_name = "nginx"
-    tls {
-      hosts       = ["*.${var.domain_name}", "${var.domain_name}"]
-      secret_name = "prod-cert"
-    }
-    rule {
-      host = "theila.${var.domain_name}"
-      http {
-
-        path {
-          path_type = "ImplementationSpecific"
-          backend {
-            service {
-              name = "theila"
               port {
                 name = "http"
               }
